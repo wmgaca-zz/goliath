f5b27e5b6649d21ab34725c5550036c4c4d7031c
Wojciech Gaca <wmgaca@gmail.com>
2014-07-13 20:36:54 +0400

    Get port from the environment and give the user a look of disapproval if needed ಠ_ಠ.

--- goliath.go
+++ goliath.go
@@ -8,7 +8,14 @@ import (
 	"os"
 )
 
+var DisapprovalLook string = "ಠ_ಠ"
+
 var ServerAddr string
+
+func ExitWithErr(message string) {
+	log.Println(DisapprovalLook, message)
+	os.Exit(-1)
+}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r, "HomeHandler()")
@@ -17,12 +24,16 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func init() {
 	ServerAddr = ":" + os.Getenv("PORT")
+	if len(ServerAddr) == 1 {
+		ExitWithErr("Server port not set.")
+	}
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler)
-
 	http.Handle("/", router)
+
+	log.Println("Runnig server on", ServerAddr)
 	http.ListenAndServe(ServerAddr, nil)
 }
