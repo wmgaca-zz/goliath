1a5dfd98fb70297acd781d11c50d679c19b8f616
Wojciech Gaca <wmgaca@gmail.com>
2014-07-14 22:24:37 +0400

    :moon: JSON responses & handling file uploads.

--- goliath.go
+++ goliath.go
@@ -7,22 +7,44 @@ import (
 	"os"
 )
 
-var serverAddr string
+const StaticDir = "./static/"
+
+var ServerAddr string
 
 func init() {
-	serverAddr = ":" + os.Getenv("PORT")
-	if len(serverAddr) == 1 {
+	// Static file
+	_, err := os.Stat(StaticDir)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(StaticDir, 0644)
+		if err != nil {
+			ExitWithErr("Can't create static dir")
+		}
+	}
+
+	// Server port
+	ServerAddr = ":" + os.Getenv("PORT")
+	if len(ServerAddr) == 1 {
 		ExitWithErr("Server port not set.")
 	}
 }
 
-func main() {
+func configureRouter() *mux.Router {
 	router := mux.NewRouter()
+
 	router.HandleFunc("/", HomeHandler).Methods("GET")
 	router.HandleFunc("/compare/", CompareHandler).Methods("POST")
 	router.HandleFunc("/image/{name}/", ImageHandler).Methods("GET")
-	http.Handle("/", router)
 
-	log.Println("Runnig server on", serverAddr)
-	http.ListenAndServe(serverAddr, nil)
+	router.PathPrefix("/static/").Handler(http.StripPrefix(
+		"/static/",
+		http.FileServer(http.Dir(StaticDir))))
+
+	return router
+}
+
+func main() {
+	http.Handle("/", configureRouter())
+
+	log.Println("Runnig server on", ServerAddr)
+	http.ListenAndServe(ServerAddr, nil)
 }
--- handlers.go
+++ handlers.go
@@ -4,6 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -13,19 +18,8 @@ func getElapsedTime(startTime time.Time) time.Duration {
 	return time.Now().Sub(startTime)
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r, "HomeHandler()")
-	startTime := time.Now()
-
-	jsonResponse, err := json.Marshal(
-		struct {
-			Result string
-			Time   time.Duration
-		}{
-			Result: "Go, baby, go!",
-			Time:   getElapsedTime(startTime),
-		})
-
+func WriteJSONResponse(w http.ResponseWriter, v interface{}) {
+	jsonResponse, err := json.Marshal(v)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,24 +27,49 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(jsonResponse))
 }
 
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r, "HomeHandler()")
+	startTime := time.Now()
+
+	WriteJSONResponse(w, struct {
+		Result string
+		Time   time.Duration
+	}{
+		Result: "Go, baby, go!",
+		Time:   getElapsedTime(startTime),
+	})
+}
+
 func CompareHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r, "CompareHandler()")
 	startTime := time.Now()
 
-	jsonResponse, err := json.Marshal(
-		struct {
-			Result bool
-			Time   time.Duration
-		}{
-			Result: true,
-			Time:   getElapsedTime(startTime),
-		})
-
+	imageFile, imageFileHeader, err := r.FormFile("image")
 	if err != nil {
-		log.Fatal(err)
+		WriteJSONResponse(w, struct{ error error }{err})
+		return
 	}
 
-	fmt.Fprintf(w, string(jsonResponse))
+	tempFile, err := ioutil.TempFile(StaticDir, imageFileHeader.Filename+"-")
+	defer tempFile.Close()
+	if err != nil {
+		WriteJSONResponse(w, struct{ error error }{err})
+		return
+	}
+
+	_, err = io.Copy(tempFile, imageFile)
+	if err != nil {
+		WriteJSONResponse(w, struct{ error error }{err})
+		return
+	}
+
+	WriteJSONResponse(w, struct {
+		FilePath string
+		Time     time.Duration
+	}{
+		FilePath: tempFile.Name(),
+		Time:     getElapsedTime(startTime),
+	})
 }
 
 func ImageHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,18 +79,11 @@ func ImageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	imageName := vars["name"]
 
-	jsonResponse, err := json.Marshal(
-		struct {
-			Result string
-			Time   time.Duration
-		}{
-			Result: imageName,
-			Time:   getElapsedTime(startTime),
-		})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprintf(w, string(jsonResponse))
+	WriteJSONResponse(w, struct {
+		Result string
+		Time   time.Duration
+	}{
+		Result: imageName,
+		Time:   getElapsedTime(startTime),
+	})
 }
